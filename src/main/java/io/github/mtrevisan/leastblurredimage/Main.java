4147de23abaa54ff1387ae97fbff6081c3ed6a35
Mauro Trevisan <mauro.simioni@targaubiest.com>
2021-07-14 08:52:43 +0200

    first commit

---
+++ src/main/java/io/github/mtrevisan/leastblurredimage/Main.java
@@ -0,0 +1,13 @@
+package io.github.mtrevisan.leastblurredimage;
+
+
+//https://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
+//https://www.researchgate.net/publication/3887632_Diatom_autofocusing_in_brightfield_microscopy_A_comparative_study
+//https://stackoverflow.com/questions/7765810/is-there-a-way-to-detect-if-an-image-is-blurry
+public class Main{
+
+	public static void main(final String[] args){
+		//TODO
+	}
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.github.mtrevisan.leastblurredimage{
+	requires org.slf4j;
+
+
+	exports io.github.mtrevisan.leastblurredimage;
+}
